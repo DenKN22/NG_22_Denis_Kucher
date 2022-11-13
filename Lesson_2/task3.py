d324025aeb7ddad17e74a112941ce46fe4ba909a
DenKN22 <kucherschool@gmail.com>
2022-11-13 21:55:29 +0200

    Update n to num

--- Lesson_2/task3.py
+++ Lesson_2/task3.py
@@ -1,5 +1,5 @@
-n = int(input("Write number"))
-for i in range(n,0,-1): #cycle from n to 1 in column
+num = int(input("Write number"))
+for i in range(num,0,-1): #cycle from num to 1 in column
     for j in range(i,0,-1): #cycle from i to 1 in 
         print(j,end = ' ')
     print()
