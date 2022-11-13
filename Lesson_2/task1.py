17b730d2c75adaff6f85245ea378339ed704cda9
DenKN22 <kucherschool@gmail.com>
2022-11-13 22:19:17 +0200

    update i

--- Lesson_2/task1.py
+++ Lesson_2/task1.py
@@ -1,9 +1,9 @@
 WordList= str(input("Write you word: "))
 dictionary = {}
 print("Count of elements", len(WordList))
-for i in WordList:
-    dictionary[i] = WordList.count(i) 
-    print(i,"-",WordList.count(i))
+for cycle in WordList:
+    dictionary[cycle] = WordList.count(cycle) 
+    print(cycle,"-",WordList.count(cycle))
 print(dictionary)
 sortDict = dict(sorted(dictionary.items()))
 print("sorted count of elements", sortDict)
--- Lesson_2/task3.py
+++ Lesson_2/task3.py
@@ -1,5 +1,5 @@
 num = int(input("Write number"))
-for i in range(num,0,-1): #cycle from num to 1 in column
-    for j in range(i,0,-1): #cycle from i to 1 in 
-        print(j,end = ' ')
+for horiz in range(num,0,-1): #cycle from num to 1 in column
+    for vert in range(horiz,0,-1): #cycle from i to 1 in 
+        print(vert,end = ' ')
     print()
